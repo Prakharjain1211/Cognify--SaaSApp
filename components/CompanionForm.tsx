65596803a010c800b108aebfeb4ff7f273c6a91e
Prakhar Jain <prakharjain1211@gmail.com>
2025-05-26 00:16:10 +0530

    add Supabase SDK and enhance Companion components with authentication and form submission logic; update package dependencies

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -26,6 +26,8 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "./ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 
 // ============================================================================
 // Form Schema Definition
@@ -57,8 +59,14 @@ const CompanionForm = () => {
   });
 
   // Form submission handler
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const companion = await createCompanion(values);
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion.");
+      redirect('/')
+    }
   };
 
   return (
